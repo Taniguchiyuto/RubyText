23ac702a23744b007ff613b9671cf977d59a5495
Taniguchiyuto <youdougukou5@gmail.com>
2025-05-13 02:51:01 +0900

    a

--- lib/src/ruby_text.dart
+++ lib/src/ruby_text.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/widgets.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:tuple/tuple.dart';
-
+import 'package:flutter/material.dart';
 import 'ruby_text_data.dart';
 
 class RubySpanWidget extends HookWidget {
